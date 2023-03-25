886bbd6f027467bf132fe2788b74e70d7a544949
Dipankar Bhaduri <110708060+DipankarBhaduri@users.noreply.github.com>
2023-03-26 00:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//your JS code here. If required.
 //your code here
-// Input array of band names
 let bandNames = ["The Beatles", "Rolling Stones", "Led Zeppelin", "Pink Floyd", "The Who", "The Doors"];
 
 // Function to remove articles from band names
